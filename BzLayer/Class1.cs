f4bed43d7c0d84e586341752a2db50f547e1f8f8
siddhisalunke123 <siddhisalunke2002@gmail.com>
2023-10-20 11:30:58 +0000

    uploaded files

---
+++ BzLayer/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Entity;
+using DataLayer;
+namespace BzLayer;
+public class MovieBz
+{
+public List<Movie> GetMovies(){
+    DataAccess dataAccess = new DataAccess();
+    return dataAccess.GetMovies();
+} 
+}
